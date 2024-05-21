2115d33839fc3712e95ffd7ccc7f936f50b6d9dd
meeklife <forsonbaah@gmail.com>
2024-05-21 21:23:13 +0100

    verify access token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,16 +1,19 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 from ..database import get_db
 from ..schema import UserLogin
 from ..models import User
 from ..utils import verify_password
+from ..oauth2 import create_access_token
 
 router = APIRouter(tags=['Authentication'])
 
 
 @router.post('/login')
-def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(
+        User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(
@@ -23,5 +26,7 @@ def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail='Invalid credentials')
 
+    access_token = create_access_token(data={"user_id": user.id})
+
     # create a token
-    return {"token": "this is a token"}
+    return {"access_token": access_token, "token_type": "bearer"}
--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 
 class PostBase(BaseModel):
@@ -36,6 +37,22 @@ class UserBase(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+    class Config:
+        from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+    class Config:
+        from_attributes = True
+
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
+    created_at: datetime
 
     class Config:
         from_attributes = True
